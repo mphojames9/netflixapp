78159b5663c5b0685f960dca419affcd778f3e4b
Mpho Matli <mphojames9@gmail.com>
2026-02-26 18:07:12 +0200

    user.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,19 +1,23 @@
-require("dotenv").config();
-const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 
-const authRoutes = require("./routes/auth");
+const userSchema = new mongoose.Schema({
+  name:{
+    type:String,
+    required:true
+  },
+  email:{
+    type:String,
+    required:true,
+    unique:true
+  },
+  password:{
+    type:String,
+    required:true
+  },
+  createdAt:{
+    type:Date,
+    default:Date.now
+  }
+});
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB Connected"))
-.catch(err=>console.log(err));
-
-app.use("/api/auth", authRoutes);
-
-app.listen(5000, ()=>console.log("Server running on port 5000"));
+module.exports = mongoose.model("User", userSchema);
