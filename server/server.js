075b51dfdd6b2d5f8618e3a3990ab23c0d5866a3
Mpho Matli <mphojames9@gmail.com>
2026-02-27 21:38:50 +0200

    fix render path issue

--- server/server.js
+++ server/server.js
@@ -80,7 +80,7 @@ app.use(express.static(path.join(__dirname, "src")));
 app.use("/script", express.static(path.join(__dirname, "script")));
 
 /* SPA fallback */
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "src", "index.html"));
 });
 
