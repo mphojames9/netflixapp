38baf0f8d5b61013f4bee8ddc26a01f72d413217
Mpho Matli <mphojames9@gmail.com>
2026-02-28 04:43:57 +0200

    login bug fix

--- script/login.js
+++ script/login.js
@@ -56,7 +56,7 @@ const email = regEmail.value;
 const password = regPassword.value;
 
 try{
-const res = await fetch("http://localhost:5000/api/auth/register",{
+const res = await fetch("/api/auth/register",{
 method:"POST",
 headers:{"Content-Type":"application/json"},
 body:JSON.stringify({name,email,password})
